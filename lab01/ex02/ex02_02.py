a0e339eb0ac5d67df93485e31dc00b8448962292
KingAzgad <phamquanghaohao123@gmail.com>
2025-02-28 15:50:21 +0700

    [add] lab01 ex01 ex02

---
+++ lab01/ex02/ex02_02.py
@@ -0,0 +1,2 @@
+ban_kinh = float(input("Nhập bán kính hình tròn: "))
+dien_tich = 3.14 * ban_kinh * ban_kinh
---
+++ lab01/ex02/ex02_03.py
@@ -0,0 +1,5 @@
+so = int(input("Nhập 1 số nguyên: "))
+if so % 2 == 0:
+    print("Đây là số chẵn")
+else:
+    print("Đây là số lẻ")
